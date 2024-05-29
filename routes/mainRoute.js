0c9f66990b81d5f8dab88c88f36a22c276f78369
tjdwns4712 <tjdwns4712@naver.com>
2024-05-29 10:39:55 +0900

    2024_05_29_1

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -8,19 +8,21 @@ const Stock = require('../models/stockNameCodeModel'); //
 
 //get main화면을 랜더링
 router.get('/', async (req, res) => {
-  res.render('mainView', { title: 'Express' })
+  res.render('mainView')
 })
 
 router.post('/search', async (req, res) => {
   const { stock_name } = req.body;
-  console.log(stock_name + " 1");
+  console.log(stock_name + " ejs 값 가져오기 정상");
 
   try {
     const stock = await Stock.findOne({ stockName: stock_name });
-    console.log(stock + " 2");
+    console.log(stock + " db로 부터 주식코드 발견");
 
     if (stock) {
-      res.json({ stockCode: stock.stockCode });
+      res.redirect('/stockShow?stockCode=' + stock.stockCode);
+      res.redirect('/stockShow?stockName=' + stock.stockName);
+      console.log(stock);
     } else {
       res.status(404).json({ message: 'Stock not found' });
     }
@@ -29,6 +31,13 @@ router.post('/search', async (req, res) => {
     res.status(500).json({ message: 'Server Error' });
   }
 });
+
+router.get('/stockShow', async (req, res) => {
+  const stockCode = req.query.stockCode;
+  const stockName = req.query.stockName;
+  console.log(stockCode);
+  res.render('stockShowView', { stockCode });
+})
 
 
 //라우터 외부 전송
